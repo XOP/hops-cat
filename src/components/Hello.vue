d39a9c188fac4b5d0f2cb058ac515432dc468f8a
XOP <wdybih@gmail.com>
2017-04-23 15:15:46 +0200

    store basics

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,8 +1,26 @@
 <template>
-    <h1>{{ msg }}</h1>
+    <section>
+        <h1>{{ msg }}</h1>
+
+        <dl>
+            <dt>items count</dt>
+            <dd>{{ itemsCount }}</dd>
+
+            <dt>catalog is empty</dt>
+            <dd>{{ isCatEmpty }}</dd>
+        </dl>
+
+        <hr>
+
+        <button @click.prevent="addItem">Add Item</button>
+        <button @click.prevent="addItemAsync">Add Item Async</button>
+        <button @click.prevent="removeItem">Remove Item</button>
+    </section>
 </template>
 
 <script>
+    import { mapState, mapGetters, mapActions } from 'vuex';
+
     export default {
         name: 'hello',
 
@@ -10,6 +28,23 @@
             return {
                 msg: 'Hops Cat'
             };
+        },
+
+        computed: {
+            ...mapState([
+                'itemsCount'
+            ]),
+            ...mapGetters([
+                'isCatEmpty'
+            ])
+        },
+
+        methods: {
+            ...mapActions([
+                'addItem',
+                'addItemAsync',
+                'removeItem'
+            ])
         }
     };
 </script>
--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,49 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const state = {
-    itemsCount: 0
+    itemsCount: 5
 };
 
-export default new Vuex.Store({state});
+const getters = {
+    isCatEmpty: state => {
+        return state.itemsCount === 0;
+    }
+};
+
+const mutations = {
+    addItem (state) {
+        state.itemsCount++;
+    },
+
+    removeItem (state) {
+        state.itemsCount--;
+    }
+};
+
+const actions = {
+    addItem ({ commit }) {
+        commit('addItem');
+    },
+
+    removeItem ({ commit, state }) {
+        if (state.itemsCount > 0) {
+            commit('removeItem');
+        }
+    },
+
+    addItemAsync ({ commit }) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                commit('addItem');
+                resolve();
+            }, 500);
+        });
+    }
+};
+
+export default new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions
+});
