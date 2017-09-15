c8a1dbc83a20674192624f3a96dd5b61c90cafcb
XOP <wdybih@gmail.com>
2017-09-15 20:56:08 +0200

    added material icons

--- src/Header.vue
+++ src/Header.vue
@@ -1,15 +1,21 @@
 <template>
-    <v-toolbar dense class="dark green-1">
+    <v-toolbar dark dense class="green darken-1">
         <v-toolbar-title>
-            <span class="image is-32x32">
+            <router-link to="/" tag="span" class="u-cur-pointer">
+                <span class="image">
                 <img src="./assets/images/icon.png" alt="Hops Cat Logo"/>
-            </span>
-            <span>Hops Cat</span>
+                </span>
+                <span>Hops Cat</span>
+            </router-link>
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat to="/">Catalogue</v-btn>
-            <v-btn flat to="/add-hops">Add Hops</v-btn>
+            <v-btn flat to="/">
+                Catalogue
+            </v-btn>
+            <v-btn flat to="/add-hops">
+                Add Hops
+            </v-btn>
         </v-toolbar-items>
     </v-toolbar>
 
@@ -60,11 +66,14 @@
 
 <style scoped>
     .image {
-        display: inline-block;
-        vertical-align: middle;
+        display: inline-flex;
+        width: 16px;
+        height: 16px;
     }
 
     .image img {
         filter: invert(100%);
+        width: 100%;
+        height: 100%;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,19 +2,21 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import VueFire from 'vuefire';
-import Buefy from 'buefy';
 
+import Buefy from 'buefy';
 import Vuetify from 'vuetify';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// buefy styles and icons
 // import 'buefy/lib/buefy.css';
-
 import './assets/styles/font-awesome.min.css';
 
+// vuetify styles and icons
 import 'vuetify/dist/vuetify.min.css';
+import './assets/styles/material-icons.css';
 
 import './main.scss';
 
