e161875742e878d7b829cbd35c70d8de3612ba1f
XOP <wdybih@gmail.com>
2017-12-19 18:59:54 +0100

    update hops fixtures

--- src/fixtures/items.js
+++ src/fixtures/items.js
@@ -11,7 +11,6 @@ export default [
             "nz"
         ],
         similar: [
-            "Columbus",
             "Amarillo"
         ],
         styles: [
@@ -68,7 +67,7 @@ export default [
             min: 4,
             max: 8
         },
-        notes: "One of the most popular American hops, extremely versatile"
+        notes: "Originating from the USDA-ARA breeding program, Cascade is an aroma-type cultivar bred in 1956 and released in 1972. It was developed by open pollination of a Fuggle seedling. Cascade is the most popular variety in craft brewing and is known for having a unique floral, spicy and citrus character with balanced bittering potential."
     },
 
     {
@@ -83,8 +82,7 @@ export default [
             "us"
         ],
         similar: [
-            "Cascade",
-            "Ahtanum"
+            "Cascade"
         ],
         styles: [
             "21-A",
@@ -143,6 +141,75 @@ export default [
             min: 2,
             max: 4
         },
-        notes: "Is in the top-10 hops of craft beer industry"
+        notes: "Discovered and introduced by Virgil Gamache Farms in Washington State, Amarillo® Brand VGXP01 cv. is an aroma variety that has recently become one of the top 10 varieties utilized by the craft beer industry. It features relatively high alpha acids along with extremely high levels of myrcene for citrus and grapefruit flavors. As a result, it is an excellent dual purpose variety. Amarillo demonstrates good resistance to downy and powdery mildew."
+    },
+
+    {
+        ".key": "fixture-2",
+
+        name: "Ahtanum",
+        alias: [
+            "YCR1"
+        ],
+        usage: "A",
+        country: [
+            "us"
+        ],
+        similar: [
+            "Cascade"
+        ],
+        styles: [
+            "21-A",
+            "21-B"
+        ],
+        types: [
+            "pellet",
+            "leaf/whole"
+        ],
+        shelfLife: 5,
+        aroma: {
+            primary: [
+                "citrus"
+            ],
+            secondary: [
+                "grapefruit"
+            ],
+            extra: [
+                "flower"
+            ]
+        },
+        alpha: {
+            min: 3.5,
+            max: 6.5
+        },
+        beta: {
+            min: 4,
+            max: 6
+        },
+        co: {
+            min: 30,
+            max: 34
+        },
+        oil: {
+            min: .5,
+            max: 1.7
+        },
+        myr: {
+            min: 45,
+            max: 55
+        },
+        hum: {
+            min: 12,
+            max: 22
+        },
+        car: {
+            min: 9,
+            max: 12
+        },
+        far: {
+            min: 0,
+            max: 1
+        },
+        notes: "Bred by Select Botanicals Group, Ahtanum® Brand YCR 1 is an aroma-type cultivar used for its aromatic properties and moderate bittering. The hop is named after the location where Charles Carpenter established the first hop farm east of the Cascade Mountains in Washington State in 1869."
     }
 ];
